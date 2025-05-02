e3f3944e7354b7815c77c5ee079101144b0b0868
Walker Aldridge <daldridge@lairdscomputer.com>
2025-05-01 23:29:31 -0500

    Working png2data

--- celeste-converter.py
+++ celeste-converter.py
@@ -11,6 +11,11 @@ class CelesteConverter:
         self.input_bytes = bytearray()
         self.output_bytes = bytearray()
 
+    def get_pixel(self, x: int, y: int, width: int, has_alpha: bool) -> bytearray:
+        bytes_per_pixel = 4 if has_alpha else 3
+        offset = ((y * width) + x) * bytes_per_pixel
+        return self.input_bytes[offset:offset + bytes_per_pixel]
+
     def convert(self, input_path: str, output_path: str, command: str, verbose: bool = False):
         target_is_folder = os.path.isdir(output_path)
         if os.path.isdir(input_path):
@@ -93,7 +98,53 @@ class CelesteConverter:
         image.save(output_path)
 
     def png2data(self, workfile: str, output_path: str, target_is_folder: bool, verbose: bool):
-        print("To be implemented")
+        if verbose:
+            print(f"Converting {workfile} to {output_path}{" (folder)" if target_is_folder else ""}")
+
+        image = Image.open(workfile)
+        width, height = image.size
+        has_alpha = image.mode == "RGBA"
+        self.input_bytes = bytearray(image.tobytes())
+
+        self.output_bytes += struct.pack("<ii", width, height)
+        self.output_bytes.append(1) if has_alpha else self.output_bytes.append(0)
+
+        index, offset = 0, 9
+        while index < width * height:
+            # Get color of current pixel
+            x = index % width
+            y = index // width
+            pixel = self.get_pixel(x, y, width, has_alpha)
+
+            # Look ahead at next 255 pixels for RLE
+            rle_count = 1
+            while True:
+                if index + rle_count > width * height:
+                    break  # out of bounds
+                x2  = (index + rle_count) % width
+                y2 = (index + rle_count) // width
+                pixel2 = self.get_pixel(x2, y2, width, has_alpha)
+                if pixel != pixel2:
+                    break  # hit end of color run
+                if rle_count == 255:
+                    break  # hit max RLE
+                rle_count += 1
+
+            # Write RLE count and pixel value
+            self.output_bytes.append(rle_count)
+            pixel.reverse()
+            if pixel[0] == 0:
+                self.output_bytes.append(0)
+            else:
+                self.output_bytes += pixel
+            index += rle_count
+
+        if target_is_folder:
+            filename = os.path.splitext(os.path.basename(workfile))[0] + ".data"
+            output_path = os.path.join(output_path, filename)
+
+        with open(output_path, "wb") as f:
+            f.write(self.output_bytes)
 
 
 # Main Program
